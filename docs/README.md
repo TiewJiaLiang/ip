11553880c19e9583813fc81b8d1d494588d42077
TiewJiaLiang <wizardtiew@gmail.com>
2025-01-19 14:41:37 +0800

    Added Echo

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# WizT User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/WizT.java
+++ src/main/java/WizT.java
@@ -1,10 +1,24 @@
-public class Duke {
+import java.util.Scanner;
+
+public class WizT {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("-------------------------------------");
+        System.out.println("Hello! I'm WizT");
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------------------");
+        Scanner sc = new Scanner(System.in);
+        String input1;
+
+        do {
+            input1 = sc.nextLine();
+            System.out.println("-------------------------------------");
+            System.out.println(input1);
+            System.out.println("-------------------------------------");
+        }while(!input1.equals("bye"));
+
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("-------------------------------------");
+
+
     }
 }
