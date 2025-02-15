ec0c2dae32cc706e2573f2f343f9d5ecfe88b552
TiewJiaLiang <wizardtiew@gmail.com>
2025-02-15 22:52:16 +0800

    Add more testing

---
+++ src/test/java/wizt/command/UpdateCommandTest.java
@@ -0,0 +1,36 @@
+package wizt.command;
+
+import org.junit.jupiter.api.Test;
+import wizt.task.Task;
+import wizt.task.Todo;
+import wizt.ui.WizTException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UpdateCommandTest {
+    @Test
+    public void emptyMarkDescription() {
+
+
+        String input1 = "mark";
+
+        Command command = new UpdateCommand(input1); // Assuming `tasks` is a wrapper for the list
+
+        // Assert that the WizTException is thrown
+        WizTException exception = assertThrows(WizTException.class, () -> {
+            if (input1.contains("mark")) {
+                String[] substr = input1.split(" ");
+                if (substr.length != 2) {
+                    throw new WizTException("Please enter a description!");
+                }
+
+
+            }
+        });
+
+        // Assert that the exception message is correct
+        assertEquals("Please enter a description!", exception.getMessage());
+    }
+
+}
