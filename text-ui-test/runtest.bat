9e91def7288e9a7258024c71faac650b6e1cdc66
TiewJiaLiang <wizardtiew@gmail.com>
2025-02-15 23:56:03 +0800

    Fix bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,17 +6,17 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
+
 REM compile the code into the bin folder
-for /R %%f in (..\src\main\java\*.java) do javac -cp ..\src\main\java -Xlint:none -d ..\bin %%f
+for /R %%f in (..\src\main\java\wizt\*.java) do javac -cp ..\bin -Xlint:none -d ..\bin %%f
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
-
 REM no error here, errorlevel == 0
 
-REM Run the program from the bin folder, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin wizt.ui.WizT < input.txt > ACTUAL.TXT
+REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ..\bin WizT < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
