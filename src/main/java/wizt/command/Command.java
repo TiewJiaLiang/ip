40002192f7f0805d889e919456c0d315093c9d9b
TiewJiaLiang <wizardtiew@gmail.com>
2025-02-15 23:35:22 +0800

    Fix Checkstyle

--- src/main/java/wizt/command/Command.java
+++ src/main/java/wizt/command/Command.java
@@ -18,8 +18,18 @@ public class Command {
 
     }
 
+    /**
+     *  Represent a execution of a command
+     * @param tasks
+     * @param ui
+     * @param storage
+     * @return
+     * @throws WizTException
+     */
     public String execute(TaskList tasks, Ui ui, Storage storage) throws WizTException {
-        return "Please enter a valid command! \n 1.list \n 2.mark [no.] \n 3.unmark[ no.] \n 4.delete [no.] \n 5.find [item] \n 6.bye \n 7.update [no.] \n 8.todo [task] \n 9.deadline [task][by DateTime] \n10.event[from DateTime to DateTime]";
+        return "Please enter a valid command! \n 1.list \n 2.mark [no.] \n 3.unmark[ no.]"
+                + "\n 4.delete [no.] \n 5.find [item] \n 6.bye \n 7.update [no.] \n 8.todo [task]"
+                + " \n 9.deadline [task][by DateTime] \n10.event[from DateTime to DateTime]";
     }
 
     public boolean isExit() {
